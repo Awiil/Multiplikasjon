e856533bc58b19200f727a4d7206d4481ac1275a
Awiil <awil.dishe@gokstadakademiet.no>
2023-03-17 12:27:54 +0100

    I changed simple to basic

---
+++ MyGithub/Multiplikasjon.py
@@ -0,0 +1,9 @@
+def mul(a,b):
+    if b == 0:
+        return 0
+    else:
+
+        return a * b
+result1=mul(5,2)
+result2 =mul(4,0)
+print("\t5 * 2 = ", result1, "\t4 * 0 = ", result2, "\nThis is a basic multiplication that is going to my github repo!")
